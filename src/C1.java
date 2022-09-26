3dddcb2de6da8a64c3c7aec0d17265efb508318d
destiny <destiniyabidingwisdom@gmail.com>
2022-09-26 10:23:39 +0300

    26.09.22

--- src/C1.java
+++ src/C1.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class C1 {
+public class C1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Locale dan merhaba");
+        System.out.println("2.kez merhaba");
+    }
 }
